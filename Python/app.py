2a35d6d80e61ca13704de247c89c064292c7331a
Mihai Tataru <mihai.tataru@student.manchester.ac.uk>
2018-04-29 04:22:43 +0100

    Up1

--- Python/app.py
+++ Python/app.py
@@ -11,6 +11,7 @@ app.config['MYSQL_PASSWORD'] = 'a8l8e8x8'
 app.config['MYSQL_DB'] = 'Galactikhackball'
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_PORT'] = '3306'
+app.config['MYSQL_UNIX_SOCKET'] = '/var/lib/mysql/mysql.sock'
 
 
 @app.route('/')
